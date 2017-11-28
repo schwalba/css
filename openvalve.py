5095adbd2c0345b18dabdaf99c02f0f8335c1a03
schwalba <petra.schwalbach@uni-mainz.de>
2017-11-28 16:49:05 +0100

    now it works

--- openvalve.py
+++ openvalve.py
@@ -14,8 +14,6 @@ sp2_low_set_1PV  = sp2_low_set_1.getPV()
 
 #first check pneuval PV connection
 if (sp1_high_set_1PV.getValue()):
-    ConsoleUtil.writeInfo("-----Pneuval 1-----")
-    
     label_gauge_sp1_high_set_2 = display.getWidget("Label_gauge_sp1_high_set_2")
     label_gauge_sp1_low_set_2  = display.getWidget("Label_gauge_sp1_low_set_2")
     label_gauge_sp2_high_set_2 = display.getWidget("Label_gauge_sp2_high_set_2")
@@ -32,15 +30,18 @@ if (sp1_high_set_1PV.getValue()):
     loc_sp2_high_set_1PV = loc_sp2_high_set_1.getPV()
     loc_sp2_low_set_1PV  = loc_sp2_low_set_1.getPV()
     
-    on_set     = display.getWidget("Boolean_Switch_on_set_pneuval")
-    loc_on_set = display.getWidget("Boolean_Switch_on_set_pneuval_loc")
-    bar        = display.getWidget("Progress_Bar")
-    p_get      = display.getWidget("Text_Update_p_get")
+    on_set_1     = display.getWidget("Boolean_Switch_on_set_pneuval1")
+    loc_on_set_1 = display.getWidget("Boolean_Switch_on_set_pneuval1_loc")
+    on_set_2     = display.getWidget("Boolean_Switch_on_set_pneuval2")
+    loc_on_set_2 = display.getWidget("Boolean_Switch_on_set_pneuval2_loc")
     
-    on_setPV     = on_set.getPV()
-    loc_on_setPV = loc_on_set.getPV()
-    p_getPV      = p_get.getPV()
-    barPV        = bar.getPV()
+    on_set_1PV     = on_set_1.getPV()
+    loc_on_set_1PV = loc_on_set_1.getPV()
+    on_set_2PV     = on_set_2.getPV()
+    loc_on_set_2PV = loc_on_set_2.getPV()
+    
+    p_get   = display.getWidget("Text_Update_p_get")
+    p_getPV = p_get.getPV()
     
     ConsoleUtil.writeInfo("-----Orig thresholds-----")
     #remember the original thresholds
@@ -88,19 +89,18 @@ if (sp1_high_set_1PV.getValue()):
         sp2_high_set_1PV.setValue(PVUtil.getDouble(loc_sp2_high_set_1PV))
         sp2_low_set_1PV.setValue(PVUtil.getDouble(loc_sp2_low_set_1PV))  
     
-    ConsoleUtil.writeInfo("-----loc_on_setPV-----")
-    loc_on = PVUtil.getLong(loc_on_setPV)
-    ConsoleUtil.writeInfo(str(loc_on))
-    ConsoleUtil.writeInfo(PVUtil.getString(loc_on_setPV))
-    on_setPV.setValue(PVUtil.getString(loc_on_setPV)) 
-    ConsoleUtil.writeInfo("-----on_setPV-----")
-    ConsoleUtil.writeInfo(PVUtil.getString(on_setPV)) 
-    
-    bar.setVisible(True)    
+    #ConsoleUtil.writeInfo("-----loc_on_setPV-----")
+    #loc_on = PVUtil.getLong(loc_on_setPV)
+    #ConsoleUtil.writeInfo(str(loc_on))
+    #ConsoleUtil.writeInfo(PVUtil.getString(loc_on_setPV))
+    on_set_1PV.setValue(PVUtil.getString(loc_on_set_1PV))
+    on_set_2PV.setValue(PVUtil.getString(loc_on_set_2PV)) 
+    ConsoleUtil.writeInfo("-----on_set_1-----")
+    ConsoleUtil.writeInfo(PVUtil.getString(on_set_1PV))
+    ConsoleUtil.writeInfo("-----on_set_2-----")
+    ConsoleUtil.writeInfo(PVUtil.getString(on_set_2PV)) 
+       
     p_get.setVisible(True)  
-    #if (p_get_1PV.getValue()):
-    barPV.setValue(PVUtil.getDouble(p_getPV))
-    #bar_1PV.setValue(8e-10)
     
     if ((loc_sp1_high_set_1.getValue()) and (loc_sp1_low_set_1.getValue())):
         label_sp1_high_low.setVisible(True)
@@ -113,13 +113,7 @@ if (sp1_high_set_1PV.getValue()):
         loc_input_sp2_high_low.setVisible(True)
         loc_input_sp2_low_low.setVisible(True)   
         
-#end if first check pneuval1 PV connection
-#else:
-    #ConsoleUtil.writeInfo("Pneuval 1 not connected")
 
-
-
-#if ((sp1_high_setPV.getValue()) or (sp1_high_setPV.getValue())):
 if (sp1_high_set_1PV.getValue()):
     action = display.getWidget("Action Button")
     blink  = display.getWidget("blink_action")
