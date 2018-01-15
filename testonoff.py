bbffd10e298a3ed038936a06e7a7f7c52e9049c5
schwalba <petra.schwalbach@uni-mainz.de>
2018-01-15 09:26:48 +0100

    new dir subopi

--- testonoff.py
+++ testonoff.py
@@ -1,1 +1,15 @@
 from org.csstudio.opibuilder.scriptUtil import PVUtil
+from org.csstudio.opibuilder.scriptUtil import ConsoleUtil
+
+loc_on_set_1     = display.getWidget("Boolean_Switch_on_set_pneuval1_loc")
+loc_on_set_2     = display.getWidget("Boolean_Switch_on_set_pneuval2_loc")
+
+loc_on_set_1PV = loc_on_set_1.getPV()
+loc_on_set_2PV = loc_on_set_2.getPV()
+
+loc_on_set_2PV.setValue(PVUtil.getString(loc_on_set_1PV))
+
+ConsoleUtil.writeInfo("-----loc_on_set_1-----")
+ConsoleUtil.writeInfo(PVUtil.getString(loc_on_set_1PV))
+ConsoleUtil.writeInfo("-----loc on_set_2-----")
+ConsoleUtil.writeInfo(PVUtil.getString(loc_on_set_2PV))
